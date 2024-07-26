3b0845d85e40ac1b5e01c383faa986f78cdf952e
Srijana Limbu <91357218+srijanalimbu55@users.noreply.github.com>
2024-07-26 21:16:35 +0545

    added

--- main.js
+++ main.js
@@ -28,7 +28,15 @@ const linkAction = () => {
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*=============== ADD SHADOW HEADER ===============*/
+/*=============== ADD Scroll/SHADOW HEADER ===============*/
+
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/
 
